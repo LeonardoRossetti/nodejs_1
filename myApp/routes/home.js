cd24ce2696cda3d7d80cd156b200a8e1cd3bd869
LeonardoRossetti <leoo.rossetti@gmail.com>
2017-10-17 13:03:47 -0200

    alteracao

--- myApp/routes/home.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app) {
-
-    var home = app.controllers.home;
-    app.route('/').get(home.index);
-}
--- myApp/routes/usuarios.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(app){
-
-    var usuarios = app.controllers.usuarios;
-
-    app.route('/usuarios').get(usuarios.index);
-    app.route('/usuarios/create').get(usuarios.create)
-}
