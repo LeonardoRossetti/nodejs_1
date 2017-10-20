e3d18864a3c5f6b7c73981b1ac8b7fe84de064df
LeonardoRossetti <leoo.rossetti@gmail.com>
2017-10-20 12:40:56 -0200

    Inserido model Times

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,15 @@
 var express = require('express'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser'),
-    app = express();
+    app = express(),
+    TImes = require('./models/times');
+
+//Conexão com o mondoDB
+mongoose.connect('mondogb://localhost/api', function(err){
+    if(err){
+        console.log('Erro ao conectar ao mongoDB: '+ err);
+    }
+})
 
 app.use(bodyParser());
 
@@ -13,6 +21,8 @@ var route = express.Router();
 route.get('/', function(req, res){
     res.json({message: 'API node'});
 });
+
+route.router('/times').post()
 
 app.use('/api', route);
 
---
+++ backend/models/times.js
@@ -0,0 +1,9 @@
+var mongoose  = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+var TimeSchema = new Schema({
+    nome: { type: String, required: true, trim: true }
+});
+
+module.exports = mongoose.model('Times', TimeSchema);
